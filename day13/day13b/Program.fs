b8db7cbb02309f186f01cc6983d0456f7b4893be
Travis Sunderland <travis@sunderland.io>
2018-12-23 21:15:31 -0600

    day 13 part 2

--- day13/day13b/Program.fs
+++ day13/day13b/Program.fs
@@ -8,7 +8,7 @@ type CurvedTrackType = FSlash | BSlash
 type Track = StraightTrack of StraightTrackType | CurvedTrack of CurvedTrackType | Intersection
 
 type TrackBeneath = Track
-type Place = Track of Track | Cart of (Direction * TurnDirection * TrackBeneath)
+type Place = Track of Track | Cart of (Direction * TurnDirection * TrackBeneath * bool)
 
 let readLines filePath = IO.File.ReadLines(filePath)
 
@@ -19,10 +19,10 @@ let charToPlace c =
     | '\\' -> Track (CurvedTrack BSlash) |> Some
     | '/' -> Track (CurvedTrack FSlash) |> Some
     | '+' -> Track Intersection |> Some
-    | '^' -> Cart (Up, TurnLeft, StraightTrack Vertical) |> Some
-    | '>' -> Cart (Right, TurnLeft, StraightTrack Horizontal) |> Some
-    | 'v' -> Cart (Down, TurnLeft, StraightTrack Vertical) |> Some
-    | '<' -> Cart (Left, TurnLeft, StraightTrack Horizontal) |> Some
+    | '^' -> Cart (Up, TurnLeft, StraightTrack Vertical, false) |> Some
+    | '>' -> Cart (Right, TurnLeft, StraightTrack Horizontal, false) |> Some
+    | 'v' -> Cart (Down, TurnLeft, StraightTrack Vertical, false) |> Some
+    | '<' -> Cart (Left, TurnLeft, StraightTrack Horizontal, false) |> Some
     | _   -> None
 
 let parseInput s = Array.map charToPlace (Array.ofSeq s)
@@ -43,170 +43,103 @@ let turnLeft dir =
 
 let turnCart place =
     match place with
-    | Cart (d, GoStraight, t) -> Cart (d, TurnRight, t)
-    | Cart (d, TurnLeft, t) -> Cart (turnLeft d, GoStraight, t)
-    | Cart (d, TurnRight, t) -> Cart (turnRight d, TurnLeft, t)
+    | Cart (d, GoStraight, t, m) -> Cart (d, TurnRight, t, m)
+    | Cart (d, TurnLeft, t, m) -> Cart (turnLeft d, GoStraight, t, m)
+    | Cart (d, TurnRight, t, m) -> Cart (turnRight d, TurnLeft, t, m)
     | _ -> place
 
-let directionEq place dir =
-    match (place, dir) with
-    | (Some (Cart (Down, _, _)), Down)
-    | (Some (Cart (Left, _, _)), Left)
-    | (Some (Cart (Right, _, _)), Right)
-    | (Some (Cart (Up, _, _)), Up) -> true
-    | _ -> false
-
-//let printTracks tracks =
-//    Console.CursorVisible <- false
-//    Array.iteri (fun y row ->
-//        if y < 50 then
-//            Array.iteri (fun x place ->
-//                match place with
-//                | Some (Track (StraightTrack Horizontal)) -> writeCharacter '-' x y ConsoleColor.Gray
-//                | Some (Track (StraightTrack Vertical)) -> writeCharacter '|' x y ConsoleColor.Gray
-//                | Some (Track (CurvedTrack FSlash)) -> writeCharacter '/' x y ConsoleColor.Gray
-//                | Some (Track (CurvedTrack BSlash)) -> writeCharacter '\\' x y ConsoleColor.Gray
-//                | Some (Track Intersection) -> writeCharacter '+' x y ConsoleColor.Gray
-//                | Some (Cart (Left, _, _)) -> writeCharacter '<' x y ConsoleColor.Blue
-//                | Some (Cart (Up, _, _)) -> writeCharacter '^' x y ConsoleColor.Blue
-//                | Some (Cart (Right, _, _)) -> writeCharacter '>' x y ConsoleColor.Blue
-//                | Some (Cart (Down, _, _)) -> writeCharacter 'v' x y ConsoleColor.Blue
-//                | _ -> writeCharacter ' ' x y ConsoleColor.Gray)
-//                row
-//            printfn "")
-//        tracks
-//    printfn ""
+let writeCharacter (c:char) x y color =
+    Console.SetCursorPosition(x, y)
+    Console.ForegroundColor <- color
+    Console.Write(c)
 
 let printTracks tracks =
     Console.CursorVisible <- false
     Array.iteri (fun y row ->
-            Array.iteri (fun x place ->
-                match place with
-                | Some (Track (StraightTrack Horizontal)) -> printf "%c" '-'
-                | Some (Track (StraightTrack Vertical)) -> printf "%c" '|'
-                | Some (Track (CurvedTrack FSlash)) -> printf "%c" '/'
-                | Some (Track (CurvedTrack BSlash)) -> printf "%c" '\\'
-                | Some (Track Intersection) -> printf "%c" '+'
-                | Some (Cart (Left, _, _)) -> printf "%c" '<'
-                | Some (Cart (Up, _, _)) -> printf "%c" '^'
-                | Some (Cart (Right, _, _)) -> printf "%c" '>'
-                | Some (Cart (Down, _, _)) -> printf "%c" 'v'
-                | _ -> printf "%c" ' ')
-                row
-            printfn "")
+        Array.iteri (fun x place ->
+            match place with
+            | Some (Track (StraightTrack Horizontal)) -> writeCharacter '-' x y ConsoleColor.Gray
+            | Some (Track (StraightTrack Vertical)) -> writeCharacter '|' x y ConsoleColor.Gray
+            | Some (Track (CurvedTrack FSlash)) -> writeCharacter '/' x y ConsoleColor.Gray
+            | Some (Track (CurvedTrack BSlash)) -> writeCharacter '\\' x y ConsoleColor.Gray
+            | Some (Track Intersection) -> writeCharacter '+' x y ConsoleColor.Gray
+            | Some (Cart (Left, _, _, _)) -> writeCharacter '<' x y ConsoleColor.Blue
+            | Some (Cart (Up, _, _, _)) -> writeCharacter '^' x y ConsoleColor.Blue
+            | Some (Cart (Right, _, _, _)) -> writeCharacter '>' x y ConsoleColor.Blue
+            | Some (Cart (Down, _, _, _)) -> writeCharacter 'v' x y ConsoleColor.Blue
+            | _ -> writeCharacter ' ' x y ConsoleColor.Gray)
+            row
+        printfn "")
         tracks
     printfn ""
 
 let updatePlace (tracks:Place option [][]) y x (place:Place option) : unit =
-    //let up = if y > 0 && x < tracks.[y - 1].Length then tracks.[y - 1].[x] else None
-    //let down = if y < tracks.Length - 1 && x < tracks.[y + 1].Length then tracks.[y + 1].[x] else None
-    //let left = if x > 0 then tracks.[y].[x - 1] else None
-    //let right = if x < tracks.[y].Length - 1 then tracks.[y].[x + 1] else None
-    //printTracks tracks
     match place with
-    | None -> ()
     | Some (Track _) -> ()
-    | Some (Cart (Left, td, t)) ->
+    | Some (Cart (Left, td, t, false)) ->
         match tracks.[y].[x-1] with
-        | Some (Cart (_, _, t)) -> tracks.[y].[x-1] <- Some (Track t)
+        | Some (Cart (_, _, t, _)) -> tracks.[y].[x-1] <- Some (Track t)
         | Some (Track track) ->
             tracks.[y].[x-1] <-
                 match track with
-                | CurvedTrack FSlash -> Some (Cart (Down, td, CurvedTrack FSlash))
-                | CurvedTrack BSlash -> Some (Cart (Up, td, CurvedTrack BSlash))
-                | Intersection -> Some (turnCart (Cart (Left, td, Intersection)))
-                | _ -> Some (Cart (Left, td, track))
+                | CurvedTrack FSlash -> Some (Cart (Down, td, CurvedTrack FSlash, true))
+                | CurvedTrack BSlash -> Some (Cart (Up, td, CurvedTrack BSlash, true))
+                | Intersection -> Some (turnCart (Cart (Left, td, Intersection, true)))
+                | _ -> Some (Cart (Left, td, track, true))
+        | _ -> ()
         tracks.[y].[x] <- Some (Track t)
-    | Some (Cart (Right, td, t)) ->
+    | Some (Cart (Right, td, t, false)) ->
         match tracks.[y].[x+1] with
-        | Some (Cart (_, _, t)) -> tracks.[y].[x+1] <- Some (Track t)
+        | Some (Cart (_, _, t, _)) -> tracks.[y].[x+1] <- Some (Track t)
         | Some (Track track) ->
             tracks.[y].[x+1] <-
                 match track with
-                | CurvedTrack FSlash -> Some (Cart (Up, td, CurvedTrack FSlash))
-                | CurvedTrack BSlash -> Some (Cart (Down, td, CurvedTrack BSlash))
-                | Intersection -> Some (turnCart (Cart (Right, td, Intersection)))
-                | _ -> Some (Cart (Right, td, track))
+                | CurvedTrack FSlash -> Some (Cart (Up, td, CurvedTrack FSlash, true))
+                | CurvedTrack BSlash -> Some (Cart (Down, td, CurvedTrack BSlash, true))
+                | Intersection -> Some (turnCart (Cart (Right, td, Intersection, true)))
+                | _ -> Some (Cart (Right, td, track, true))
+        | _ -> ()
         tracks.[y].[x] <- Some (Track t)
-    | Some (Cart (Up, td, t)) ->
+    | Some (Cart (Up, td, t, false)) ->
         match tracks.[y-1].[x] with
-        | Some (Cart (_, _, t)) -> tracks.[y-1].[x] <- Some (Track t)
+        | Some (Cart (_, _, t, _)) -> tracks.[y-1].[x] <- Some (Track t)
         | Some (Track track) ->
             tracks.[y-1].[x] <-
                 match track with
-                | CurvedTrack FSlash -> Some (Cart (Right, td, CurvedTrack FSlash))
-                | CurvedTrack BSlash -> Some (Cart (Left, td, CurvedTrack BSlash))
-                | Intersection -> Some (turnCart (Cart (Up, td, Intersection)))
-                | _ -> Some (Cart (Up, td, track))
+                | CurvedTrack FSlash -> Some (Cart (Right, td, CurvedTrack FSlash, true))
+                | CurvedTrack BSlash -> Some (Cart (Left, td, CurvedTrack BSlash, true))
+                | Intersection -> Some (turnCart (Cart (Up, td, Intersection, true)))
+                | _ -> Some (Cart (Up, td, track, true))
+        | _ -> ()
         tracks.[y].[x] <- Some (Track t)
-    | Some (Cart (Down, td, t)) ->
+    | Some (Cart (Down, td, t, false)) ->
         match tracks.[y+1].[x] with
-        | Some (Cart (_, _, t)) -> tracks.[y+1].[x] <- Some (Track t)
+        | Some (Cart (_, _, t, _)) -> tracks.[y+1].[x] <- Some (Track t)
         | Some (Track track) ->
             tracks.[y+1].[x] <-
                 match track with
-                | CurvedTrack FSlash -> Some (Cart (Left, td, CurvedTrack FSlash))
-                | CurvedTrack BSlash -> Some (Cart (Right, td, CurvedTrack BSlash))
-                | Intersection -> Some (turnCart (Cart (Down, td, Intersection)))
-                | _ -> Some (Cart (Down, td, track))
+                | CurvedTrack FSlash -> Some (Cart (Left, td, CurvedTrack FSlash, true))
+                | CurvedTrack BSlash -> Some (Cart (Right, td, CurvedTrack BSlash, true))
+                | Intersection -> Some (turnCart (Cart (Down, td, Intersection, true)))
+                | _ -> Some (Cart (Down, td, track, true))
+        | _ -> ()
         tracks.[y].[x] <- Some (Track t)
-    //| None -> place
-    //| Some (Cart (Right, _, t)) ->
-    //    match (up, down, left, right) with
-    //    | (_, _, _, Some (Cart _)) -> Track t |> Some
-    //    | _ ->
-    //| Some (Cart (_, _, t)) ->
-    //    match (up, down, left, right) with
-    //    | (Some (Cart (Down, _, _)), _, _, _)
-    //    | (_, Some (Cart _), _, _)
-    //    | (_, _, Some (Cart (Right, _, _)), _)
-    //    |  -> Track t |> Some
-    //    | _ -> Track t |> Some
-    //| Some (Track (StraightTrack Horizontal)) ->
-    //    match (left, right) with
-    //    | (Some (Cart (Right, _, _)), Some (Cart (Left, _, _))) -> Track (StraightTrack Horizontal) |> Some
-    //    | (Some (Cart (Right, td, t)), _) -> Cart (Right, td, StraightTrack Horizontal) |> Some
-    //    | (_, Some (Cart (Left, td, t))) -> Cart (Left, td, StraightTrack Horizontal) |> Some
-    //    | _ -> place
-    //| Some (Track (StraightTrack Vertical)) ->
-    //    match (up, down) with
-    //    | (Some (Cart (Down, _, _)), Some (Cart (Up, _, _))) -> Track (StraightTrack Vertical) |> Some
-    //    | (Some (Cart (Down, td, t)), _) -> Cart (Down, td, StraightTrack Vertical) |> Some
-    //    | (_, Some (Cart (Up, td, t))) -> Cart (Up, td, StraightTrack Vertical) |> Some
-    //    | _ -> place
-    //| Some (Track Intersection) ->
-    //    match (up, down, left, right) with
-    //    | _ when List.filter id [directionEq up Down; directionEq down Up; directionEq left Right; directionEq right Left] |> List.length > 1 -> Track Intersection |> Some
-    //    | (Some (Cart (Down, td, _)), _, _, _) -> turnCart (Cart (Down, td, Intersection)) |> Some
-    //    | (_, Some (Cart (Up, td, _)), _, _) -> turnCart (Cart (Up, td, Intersection)) |> Some
-    //    | (_, _, Some (Cart (Right, td, _)), _) -> turnCart (Cart (Right, td, Intersection)) |> Some
-    //    | (_, _, _, Some (Cart (Left, td, _))) -> turnCart (Cart (Left, td, Intersection)) |> Some
-    //    | _ -> Track Intersection |> Some
-    //| Some (Track (CurvedTrack FSlash)) ->
-    //    match (up, down, left, right) with
-    //    | (Some (Cart (Down, _, _)), _, Some (Cart (Right, _, _)), _)
-    //    | (_, Some (Cart (Up, _, _)), _, Some (Cart (Left, _, _))) -> Track (CurvedTrack FSlash) |> Some
-    //    | (Some (Cart (Down, td, _)), _, _, _) -> Cart (Left, td, CurvedTrack FSlash) |> Some
-    //    | (_, Some (Cart (Up, td, _)), _, _) -> Cart (Right, td, CurvedTrack FSlash) |> Some
-    //    | (_, _, Some (Cart (Right, td, _)), _) -> Cart (Up, td, CurvedTrack FSlash) |> Some
-    //    | (_, _, _, Some (Cart (Left, td, _))) -> Cart (Down, td, CurvedTrack FSlash) |> Some
-    //    | _ -> Track (CurvedTrack FSlash) |> Some
-    //| Some (Track (CurvedTrack BSlash)) ->
-    //    match (up, down, left, right) with
-    //    | (Some (Cart (Down, _, _)), _, _, Some (Cart (Left, _, _)))
-    //    | (_, Some (Cart (Up, _, _)), Some (Cart (Right, _, _)), _) -> Track (CurvedTrack BSlash) |> Some
-    //    | (Some (Cart (Down, td, _)), _, _, _) -> Cart (Right, td, CurvedTrack BSlash) |> Some
-    //    | (_, Some (Cart (Up, td, _)), _, _) -> Cart (Left, td, CurvedTrack BSlash) |> Some
-    //    | (_, _, Some (Cart (Right, td, _)), _) -> Cart (Down, td, CurvedTrack BSlash) |> Some
-    //    | (_, _, _, Some (Cart (Left, td, _))) -> Cart (Up, td, CurvedTrack BSlash) |> Some
-    //    | _ -> Track (CurvedTrack BSlash) |> Some
+    | _ -> ()
 
 let updateRow (tracks:Place option [][]) y row =
     Array.iteri (updatePlace tracks y) row
 
 let tick (tracks:Place option [][]) =
     Array.iteri (updateRow tracks) tracks
-    tracks
+    Array.map
+        (fun row ->
+            Array.map
+                (fun place ->
+                    match place with
+                    | Some (Cart (d, td, t, _)) -> Some (Cart (d, td, t, false))
+                    | p -> p)
+                row)
+        tracks
 
 let findSome = Array.reduce (fun x r ->
     match (r, x) with
@@ -238,11 +171,6 @@ let checkCartsLeft tracks =
         tracks
         |> Array.sum
 
-let writeCharacter (c:char) x y color =
-    Console.SetCursorPosition(x, y)
-    Console.ForegroundColor <- color
-    Console.Write(c)
-
 let rec run (tracks:Place option [][]) =
     let cartsLeft = checkCartsLeft tracks
     if cartsLeft % 2 = 0 then
@@ -257,8 +185,6 @@ let rec run (tracks:Place option [][]) =
 let main argv =
     let lines = readLines argv.[0]
     let tracks = Array.create (Seq.length lines) Array.empty
-
-    Console.Clear()
 
     Seq.iteri (fun i line -> tracks.[i] <- parseInput line) lines
 
