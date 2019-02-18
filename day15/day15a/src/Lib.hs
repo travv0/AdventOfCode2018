ab37cd9894e32cb49f24583f2def791a3a4513af
Travis Sunderland <travis@sunderland.io>
2019-02-18 13:01:38 -0600

    continue work on day 15a

--- day15/day15a/src/Lib.hs
+++ day15/day15a/src/Lib.hs
@@ -1,26 +1,61 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Lib
   ( runGame
   )
 where
 
 import           Control.Monad.State
-import           Data.Vector                    ( Vector )
+import           Control.Monad.State.Class
+import           Data.HashSet                   ( HashSet )
+import           Data.Hashable
+import           Data.Maybe                     ( catMaybes )
+import           Data.Vector                    ( Vector, (!?) )
+import           GHC.Generics
+import qualified Data.HashSet                  as H
 import qualified Data.Vector                   as V
 
-data Cell = Wall | Cavern | Goblin | Elf
-  deriving (Eq, Show)
+data Cell = Cell Entity Pos
+  deriving (Eq, Show, Generic)
+
+type Pos = (Int, Int)
+
+data Entity = Wall | Cavern | Goblin | Elf
+  deriving (Eq, Show, Generic)
 
 type Map = Vector (Vector Cell)
+
+instance Hashable Entity
+instance Hashable Cell
 
 runGame :: String -> Integer
 runGame = undefined
 
 parseInput :: String -> Map
-parseInput = V.fromList . map (V.fromList . map charToCell) . lines
+parseInput =
+  V.fromList . mapi (\y -> V.fromList . mapi (`charToEntity` y)) . lines
 
-charToCell :: Char -> Cell
-charToCell '#' = Wall
-charToCell '.' = Cavern
-charToCell 'G' = Goblin
-charToCell 'E' = Elf
-charToCell _   = error "Invalid character in input string"
+mapi :: (Int -> a -> b) -> [a] -> [b]
+mapi f = zipWith f [0 ..]
+
+charToEntity :: Int -> Int -> Char -> Cell
+charToEntity x y '#' = Cell Wall (x, y)
+charToEntity x y '.' = Cell Cavern (x, y)
+charToEntity x y 'G' = Cell Goblin (x, y)
+charToEntity x y 'E' = Cell Elf (x, y)
+charToEntity _ _ _   = error "Invalid character in input string"
+
+neighbors :: (MonadState Map m) => Cell -> m (HashSet Cell)
+neighbors (Cell _ (x, y)) = do
+  ns <- sequence
+    [getCell x (y - 1), getCell (x - 1) y, getCell (x + 1) y, getCell x (y + 1)]
+  return $ H.fromList $ catMaybes ns
+
+getCell :: (MonadState Map m) => Int -> Int -> m (Maybe Cell)
+getCell x y = do
+  m <- get
+  return $ (m !? y) >>= (!? x)
+
+distance :: Cell -> Cell -> Int
+distance (Cell _ (x1, y1)) (Cell _ (x2, y2)) = abs (x2 - x1) + abs (y2 - y1)
