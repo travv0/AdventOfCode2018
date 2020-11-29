0b78976ed5e79a260bce5af067740b142d2ec1d9
Travis <tssund93@gmail.com>
2020-11-29 16:41:05 -0600

    small day 4 refactor

--- day04/src/Lib.hs
+++ day04/src/Lib.hs
@@ -4,7 +4,6 @@ import Control.Lens (at, non, (%~), (&))
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 import Data.List (isInfixOf, sort)
-import Data.Maybe (fromJust)
 import Text.Regex.TDFA ((=~))
 
 countSleep :: [String] -> IntMap (IntMap Int)
@@ -13,36 +12,43 @@ countSleep entries =
      in countSleep' entries' Nothing Nothing IntMap.empty
   where
     countSleep' :: [String] -> Maybe Int -> Maybe Int -> IntMap (IntMap Int) -> IntMap (IntMap Int)
-    countSleep' entries' guardId sleepStartTime counts =
-        case entries' of
-            [] -> counts
-            entry : rest
-                | '#' `elem` entry ->
-                    let (_, _, _, [i]) =
-                            entry =~ "#([0-9]+)" :: (String, String, String, [String])
-                     in countSleep' rest (Just (read i)) sleepStartTime counts
-                | "falls asleep" `isInfixOf` entry ->
-                    let (_, _, _, [time]) =
-                            entry =~ ":([0-9][0-9])\\]" :: (String, String, String, [String])
-                     in countSleep' rest guardId (Just (read time)) counts
-                | otherwise ->
-                    let (_, _, _, [time]) =
-                            entry =~ ":([0-9][0-9])\\]" :: (String, String, String, [String])
-                     in countSleep'
-                            rest
-                            guardId
-                            Nothing
-                            $ foldr
-                                ( \time' counts' ->
-                                    counts'
-                                        & at (fromJust guardId)
-                                            . non IntMap.empty
-                                            . at time'
-                                            . non 0
-                                        %~ succ
-                                )
-                                counts
-                                [fromJust sleepStartTime .. read time - 1]
+    countSleep' [] _ _ counts = counts
+    countSleep' (entry : rest) guardId sleepStartTime counts
+        | '#' `elem` entry =
+            let (_, _, _, [i]) =
+                    entry =~ "#([0-9]+)" :: (String, String, String, [String])
+             in countSleep' rest (Just (read i)) sleepStartTime counts
+        | "falls asleep" `isInfixOf` entry =
+            let (_, _, _, [time]) =
+                    entry =~ ":([0-9][0-9])\\]" :: (String, String, String, [String])
+             in countSleep' rest guardId (Just (read time)) counts
+    countSleep' (entry : rest) (Just guardId) (Just sleepStartTime) counts =
+        let (_, _, _, [time]) =
+                entry =~ ":([0-9][0-9])\\]" :: (String, String, String, [String])
+         in countSleep'
+                rest
+                (Just guardId)
+                Nothing
+                $ foldr
+                    ( \time' counts' ->
+                        counts'
+                            & at guardId
+                                . non IntMap.empty
+                                . at time'
+                                . non 0
+                            %~ succ
+                    )
+                    counts
+                    [sleepStartTime .. read time - 1]
+    countSleep' (entry : _) guardId sleepStartTime _ =
+        error $
+            "Invalid state: guardId = "
+                <> show guardId
+                <> ", sleepStartTime = "
+                <> show sleepStartTime
+                <> ", entry =\""
+                <> entry
+                <> "\""
 
 mostMinutesId :: IntMap (IntMap Int) -> Int
 mostMinutesId =
